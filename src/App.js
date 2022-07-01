58ce3555fd69e8a18f2e9e4ce865012eda20240c
adasso <17396091+Adasso@users.noreply.github.com>
2022-06-30 23:41:27 -0500

    add styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import logo from './logo.png';
 import './App.css';
-import { Button, Card, CardContent, TextField } from '@mui/material';
+import { Button, Card, CardContent, Grid, TextField } from '@mui/material';
 import { Container } from '@mui/system';
-import { Link } from "react-router-dom";
-
 
 const LoginForm = () =>{
   return <Card sx={{minWidth: 275}}  className="LoginCard">
   <CardContent className="LoginContent">
-    <div>
-      Bienvenido.
+    <div className='LoginTitle'>
+     Bienvenido
     </div>
-    <div>
+    <div className="TextInputContainer">
      <TextField id="outlined-basic" label="Email" variant="outlined" className="TextFieldStyle"/>
     </div>
-    <div>
+    <div className="TextInputContainer ExtraPadding">
       <TextField id="outlined-basic" label="Contraseña" variant="outlined" className="TextFieldStyle"/>
     </div>
-    <div>
+    <div className="ButtonContainer">
       <Button variant="contained" className="PrimaryButtonStyle">Sign In</Button>
     </div>
-    <div>
+    <div className="ButtonContainer">
       <Button variant="outlined" className="SecondaryButtonStyle">Sign Up</Button>
     </div>
     
@@ -39,10 +37,17 @@ const LoginLogo = () =>{
 
 const Login = () =>{
   return (
-    <Container className="LoginContainer" maxWidth="sm">
-      <LoginLogo></LoginLogo>
-      <LoginForm></LoginForm>
-    </Container>
+    <Grid container spacing={0} align="center" justify="center" alignItems="center" height="70vh">
+      <Grid item xs={2}></Grid>
+      <Grid item xs={8}>
+        <Container className="LoginContainer" maxWidth="sm">
+          <LoginLogo></LoginLogo>
+          <LoginForm></LoginForm>
+        </Container>
+      </Grid>
+      <Grid item xs={2}></Grid>
+    </Grid>
+    
   );
 }
 
